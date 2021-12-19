00afd36edb5c8ad082c84a32c8a553ea1c2c4229
Hernan G <hernan.gomez.20@alumnos.uda.cl>
2021-12-18 21:33:24 -0300

    se puede buscar segun el nombre

--- gestiondatos.php
+++ gestiondatos.php
@@ -34,58 +34,12 @@
     <div class="container">
       <h2>Gestión de datos</h2>
     </div>
-    <div class="container">
-      <table class="table" >
-        <thead class="table-success table-striped">
-          <tr>
-            <th>ID</th>
-            <th>Nombre completo</th>
-            <th>Correo</th>
-            <th>Contraseña</th>
-            <th>Usuario</th>
-            <th>RUT</th>
-            <th>Dirección</th>
-            <th>Sexo</th>
-            <th>Fecha de nacimiento</th>
-            <th>Edad</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-            while($row=mysqli_fetch_array($query)){
-          ?>
-          <tr>
-            <th><?php  echo $row['id']?></th>
-            <th><?php  echo $row['nombrecompleto']?></th>
-            <th><?php  echo $row['correo']?></th>
-            <th><?php  echo $row['contrasena']?></th>
-            <th><?php  echo $row['usuario']?></th>
-            <th><?php  echo $row['rut']?></th>
-            <th><?php  echo $row['direccion']?></th>
-            <th><?php  echo $row['sexo']?></th>
-            <th><?php  echo $row['fechanac']?></th>
-            <th><?php  echo $row['edad']?></th>
-            <th><a href="modificar.php?id=<?php echo $row['id'] ?>" class="btn btn-info">Modificar</a></th>
-            <th><a href="delete.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">Eliminar</a></th>                                        
-          </tr>
-          <?php 
-            }
-          ?>
-        </tbody>
-      </table>
-      <center><div class="container">
+    <center><div class="container">
         <div class="container mt-5">
               <div class="col-12">
                 <form method="POST" action="gestiondatos.php">
                   <div class="mb-3">
-                    <select  class="form-select" id="buscar" name="buscar" placeholder="Sexo">
-                                <option selected="true" disabled="disabled">Indique un sexo para buscar/filtrar</option>
-                                <option>Masculino</option>
-                                <option>Femenino</option>
-                                <option>Otros</option>
-                    </select>
+                    <input type="text" class="form-control mb-3" name="buscar" id="buscar" placeholder="Ingrese su busqueda">
                   </div>
                   <button type="text" class="btn btn-primary">Buscar</button>
                 </form>
@@ -94,15 +48,51 @@
             <div class="card col-12 mt-5">
               <div class="card-body">
                 <?php 
-                $busqueda=mysqli_query($con,"SELECT * FROM tabla WHERE sexo LIKE LOWER('%".$_POST["buscar"]."%')");
+                $busqueda=mysqli_query($con,"SELECT * FROM tabla WHERE nombrecompleto LIKE LOWER('%".$_POST["buscar"]."%')");
                 $numero = mysqli_num_rows($busqueda); ?>
-                <h5 class="card-tittle">Resultado (<?php echo $numero; ?>)</h5>
+                <h5 class="card-tittle">Resultados (<?php echo $numero; ?>)</h5>
                 <?php while ($resultado = mysqli_fetch_array($busqueda)){ ?>
-                <p class="card-tittle"><?php echo $resultado["nombrecompleto"]; ?> - <?php echo $resultado["sexo"]; ?></p>
-                <?php } ?>
+                  <div class="container">
+                    <table class="table" >
+                      <thead class="table-success table-striped">
+                        <tr>
+                          <th>ID</th>
+                          <th>Nombre completo</th>
+                          <th>Correo</th>
+                          <th>Contraseña</th>
+                          <th>Usuario</th>
+                          <th>RUT</th>
+                          <th>Dirección</th>
+                          <th>Sexo</th>
+                          <th>Fecha de nacimiento</th>
+                          <th>Edad</th>
+                          <th></th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th><?php  echo $resultado['id']?></th>
+                          <th><?php  echo $resultado['nombrecompleto']?></th>
+                          <th><?php  echo $resultado['correo']?></th>
+                          <th><?php  echo $resultado['contrasena']?></th>
+                          <th><?php  echo $resultado['usuario']?></th>
+                          <th><?php  echo $resultado['rut']?></th>
+                          <th><?php  echo $resultado['direccion']?></th>
+                          <th><?php  echo $resultado['sexo']?></th>
+                          <th><?php  echo $resultado['fechanac']?></th>
+                          <th><?php  echo $resultado['edad']?></th>
+                          <th><a href="modificar.php?id=<?php echo $resultado['id'] ?>" class="btn btn-info">Modificar</a></th>
+                          <th><a href="delete.php?id=<?php echo $resultado['id'] ?>" class="btn btn-danger">Eliminar</a></th>                                        
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <?php } ?>
               </div>
             </div>
     </div></center>
+    
     <div class="container">
         <footer class="py-3 my-4">
           <ul class="nav justify-content-center border-bottom pb-3 mb-3">
